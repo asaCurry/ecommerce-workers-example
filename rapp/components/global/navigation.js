46ca5588f4874121b2703450e26479bc1eaa0abe
asaCurry <asacurry80@gmail.com>
2021-09-07 09:18:13 -0400

    navigation cosmetic change

--- rapp/components/global/navigation.js
+++ rapp/components/global/navigation.js
@@ -19,13 +19,12 @@ const navItems = [
 export default function Navigation() {
 
     const cartCount = useSelector(selectCartCount)
-    console.log(cartCount)
 
     return (
         <div className="flex items-center justify-center">
             <div className="inner-wrapper flex flex-row items-center justify-center py-12">
-                {navItems.map((item) => {
-                    return <div className="text-accentone px-4 font-medium">
+                {navItems.map((item, itemInd) => {
+                    return <div className="text-accentone px-4 font-medium" key={itemInd}>
                                 <a href={item.slug}>{item.name}</a>
                             </div>
                 })}
